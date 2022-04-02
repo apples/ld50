78a673f980234cf60bfaa4c008c831ee2570cd5e
Apples <dbralir@gmail.com>
2022-04-02 05:06:45 -0500

    Player controller leg spring logic

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,12 +9,23 @@ public class PlayerController : MonoBehaviour
     public GameObject cameraBoom;
     public float sensitivity = 1;
     public float speed = 1;
+    public float jumpSpeed;
+
+    public float legRideHeight;
+    public float legRayDistance;
+    public float legSpringConstant;
+    public float legDamping;
 
     private new Rigidbody rigidbody;
 
     private Vector2 movementInput;
     private Vector2 aimInput;
     private bool jumpInput;
+    private bool isOnGround;
+    private Rigidbody groundRigidbody;
+    private bool isJumping;
+
+    private Vector3 debug_legSpringForce;
 
     void Awake()
     {
@@ -42,12 +53,50 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        ProcessLegs();
         ProcessMouseInput();
         ProcessMovementInput();
 
         movementInput = default;
         aimInput = default;
         jumpInput = false;
+    }
+
+    private void ProcessLegs()
+    {
+        var rayDir = Vector3.down;
+
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.1f, 0), rayDir, out RaycastHit hitInfo, legRayDistance + 0.1f))
+        {
+            var compressionDistance = (hitInfo.distance - 0.1f) - legRideHeight;
+
+            var selfVel = Vector3.Dot(rayDir, rigidbody.velocity);
+            var groundVel = hitInfo.rigidbody != null ? Vector3.Dot(rayDir, hitInfo.rigidbody.velocity) : 0f;
+            var relVel = selfVel - groundVel;
+
+            var springForce = compressionDistance * legSpringConstant - relVel * legDamping;
+
+            // only apply downward force if not jumping, but always apply upward force
+            if (springForce < 0 || !isJumping)
+            {
+                rigidbody.AddForce(rayDir * springForce);
+                debug_legSpringForce = rayDir * springForce;
+            }
+
+            // reset isJumping if we're travelling downwards towards ground
+            if (springForce < 0)
+            {
+                isJumping = false;
+            }
+
+            isOnGround = true;
+            groundRigidbody = hitInfo.rigidbody;
+        }
+        else
+        {
+            isOnGround = false;
+            groundRigidbody = null;
+        }
     }
 
     private void ProcessMovementInput()
@@ -61,9 +110,14 @@ public class PlayerController : MonoBehaviour
         // jump
 
         //float j = Input.GetAxisRaw("Jump");
-        if (jumpInput && Physics.Raycast(transform.position, new Vector3(0, -1, 0), 1.1f))
+        if (jumpInput && isOnGround && !isJumping)
         {
-            rigidbody.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            var vel = rigidbody.velocity;
+            vel.y =
+                jumpSpeed +
+                (groundRigidbody != null ? groundRigidbody.velocity.y : 0);
+            rigidbody.velocity = vel;
+            isJumping = true;
         }
     }
 
