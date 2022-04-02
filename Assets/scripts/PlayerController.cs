5f705223dd929e98ed74538416b9a75d9c4b42b5
Apples <dbralir@gmail.com>
2022-04-02 02:52:47 -0500

    Improved player camera and input

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,30 +6,82 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 0.01f;
-    public float sensitivity = 2.0f;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject cameraBoom;
+    public float sensitivity = 1;
+    public float speed = 1;
+
+    private new Rigidbody rigidbody;
+
+    private Vector2 movementInput;
+    private Vector2 aimInput;
+    private bool jumpInput;
+
+    void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        Debug.Assert(rigidbody != null);
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Debug.Assert(cameraBoom != null);
+    }
+
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        float mouseX = Input.GetAxisRaw("Mouse X");
+        float mouseY = Input.GetAxisRaw("Mouse Y");
+        var jumpPressed = Input.GetKeyDown(KeyCode.Space);
+
+        movementInput += new Vector2(h, v) * speed * Time.deltaTime;
+        aimInput += new Vector2(mouseX, mouseY) * sensitivity * Time.deltaTime;
+        if (jumpPressed) jumpInput = true;
+    }
+
+    void FixedUpdate()
+    {
+        ProcessMouseInput();
+        ProcessMovementInput();
+
+        movementInput = default;
+        aimInput = default;
+        jumpInput = false;
+    }
+
+    private void ProcessMovementInput()
+    {
+        // move
+
+        transform.position +=
+            transform.forward * movementInput.y +
+            transform.right * movementInput.x;
+
+        // jump
+
         //float j = Input.GetAxisRaw("Jump");
-        float x = Input.GetAxisRaw("Mouse X");
-        float y = Input.GetAxisRaw("Mouse Y");
-
-        gameObject.transform.position = new Vector3 (transform.position.x + (h * speed), gameObject.transform.position.y,
-            transform.position.z + (v * speed));
-
-        if(Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, new Vector3(0, -1, 0), 1.0f)){
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+        if (jumpInput && Physics.Raycast(transform.position, new Vector3(0, -1, 0), 1.1f))
+        {
+            rigidbody.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
         }
-        
-        gameObject.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(-y * sensitivity, x * sensitivity, 0);
+    }
+
+    private void ProcessMouseInput()
+    {
+        // rotate player left-right
+
+        var rotation = transform.localEulerAngles;
+        rotation.y += aimInput.x;
+        transform.localEulerAngles = rotation;
+
+        // rotate camera boom up-down
+
+        var camRotation = cameraBoom.transform.localEulerAngles;
+        camRotation.x -= aimInput.y;
+        if (camRotation.x < 0) camRotation.x += 360;
+        if (camRotation.x <= 180 && camRotation.x > 90) camRotation.x = 90;
+        if (camRotation.x > 180 && camRotation.x < 270) camRotation.x = 270;
+        cameraBoom.transform.localEulerAngles = camRotation;
     }
 }
