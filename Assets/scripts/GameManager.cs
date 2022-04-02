1997ba806a99caba8e6d314839befbd4649cba6b
WiredOverload <mike.everywhere@gmail.com>
2022-04-02 18:42:13 -0500

    added crate and balloon spawns

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject cloudPrefab;
+    public GameObject cratePrefab;
+    public GameObject balloonPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -12,6 +14,14 @@ public class GameManager : MonoBehaviour
         for(int i=0; i < 10000; i++){
             Vector3 coord = new Vector3(Random.Range(-300.0f, 300.0f), Random.Range(0, i/20), Random.Range(-300.0f, 300.0f));
             Instantiate(cloudPrefab, coord, Quaternion.identity);
+
+            if(Random.value > .9f){
+                Instantiate(cratePrefab, coord + new Vector3(0, 1, 0), Quaternion.identity);
+            }
+
+            if(Random.value > .95f){
+                Instantiate(balloonPrefab, coord + new Vector3(0, 3, 0), Quaternion.identity);
+            }
 
             while(Random.value > .3f){
                 float dir = Random.Range(-1.0f, 1.0f);
