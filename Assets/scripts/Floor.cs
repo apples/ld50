4a50d4cda8ca6d23c951688d9bedbd365f33bd01
Jon Lamont <JonLamont13@Gmail.com>
2022-04-03 19:59:30 -0400

    Display the lose reason on Game Over

--- Assets/scripts/Floor.cs
+++ Assets/scripts/Floor.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Floor : MonoBehaviour
 {
+    public TextMeshProUGUI tmpText;
+    private IEnumerator coroutine;
+
     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Raft")){
-            Debug.Log("End game!");
+        coroutine = EndAfterSeconds(3);
+        if(tmpText == null && (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Raft"))){
             SceneManager.LoadScene("endScene");
+        }else if(other.gameObject.CompareTag("Player")){
+            tmpText.text = "Game Over!\nSadie Hit the Ground...";
+            tmpText.gameObject.SetActive(true);
+            StartCoroutine(coroutine);
+        }else if(other.gameObject.CompareTag("Raft")){
+            tmpText.text = "Game Over!\nThe Raft Hit the Ground...";
+            tmpText.gameObject.SetActive(true);
+            StartCoroutine(coroutine);
         }
+    }
+
+    private IEnumerator EndAfterSeconds(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("endScene");
     }
 }
