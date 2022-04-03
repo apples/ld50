3f38340ac4859326cbccc0441e532213e82d793b
Jon Lamont <JonLamont13@Gmail.com>
2022-04-02 20:09:34 -0400

    Altitude Display

---
+++ Assets/scripts/AltitudeDisplay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.UI;
+
+
+public class AltitudeDisplay : MonoBehaviour
+{
+    [SerializeField] private Transform raftHeight; 
+    [SerializeField] private Transform playerHeight; 
+    [SerializeField] private Transform player; 
+    [SerializeField] private Transform raft; 
+
+    public float minHeight = 0;
+    public float maxHeight = 500;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHeight.localScale = new Vector3(1, player.position.y / maxHeight, 1);
+        raftHeight.localScale = new Vector3(1, raft.position.y / maxHeight, 1);
+    }
+}
