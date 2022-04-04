5bd7ce25fbec172874a6c8bd9c450608e4044525
WiredOverload <mike.everywhere@gmail.com>
2022-04-03 22:31:28 -0500

    added cloud shifting process

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public GameObject cratePrefab;
     public GameObject balloonPrefab;
     public GameObject player;
-    private int currentLevel = 0;
+    private int highestY = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -16,23 +16,69 @@ public class GameManager : MonoBehaviour
         createClouds(0, 3.0f);
         createClouds(1, 2.5f);
         createClouds(2, 2.0f);
-        //createClouds(3, 1.5f);
+        createClouds(3, 1.5f);
         //createClouds(4, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y / 100 > currentLevel + 1){
-            currentLevel++;
-            if(currentLevel <= 2){
-                createClouds(currentLevel + 2, 2.0f - (0.5f * currentLevel));
-            }
-            else if(currentLevel <= 11){
-                createClouds(currentLevel + 2, 1.0f - (0.1f * (currentLevel - 2)));
-            }
-            else{
-                createClouds(currentLevel + 2, 0.1f);
+        if((int)player.transform.position.y > highestY){
+            highestY++;
+            
+            var availableObjects = Physics.OverlapBox(
+                new Vector3(0, (-50 + player.transform.position.y) / 2, 0),
+                new Vector3(300, (50 + player.transform.position.y)/2, 300),
+                Quaternion.identity,
+                1<<7);
+            int nextCloudSlot = 0;
+            for(int i = 0; i < 10; i++){
+                Vector3 coord = new Vector3(Random.Range(-300.0f, 300.0f), Random.Range((highestY + 300) - 50, (highestY + 300) + 50), Random.Range(-300.0f, 300.0f));
+                availableObjects[nextCloudSlot].transform.parent.parent.SetPositionAndRotation(coord, Quaternion.identity);
+                nextCloudSlot++;
+
+
+                float spawning = Random.value;
+                if(spawning > .9f){
+                    Instantiate(cratePrefab, coord + new Vector3(0, 1, 0), Quaternion.identity);
+                }
+                else if(spawning < .1f){
+                    Instantiate(balloonPrefab, coord + new Vector3(0, 3, 0), Quaternion.identity);
+                }
+
+                while(Random.value > .3f){
+                    float dir = Random.Range(-1.0f, 1.0f);
+                    if(dir > 0){
+                        if(dir > .5f){
+                            coord += new Vector3(5, 0, 0);
+                        }
+                        else{
+                            coord += new Vector3(-5, 0, 0);
+                        }
+                    }else{
+                        if(dir < -.5f){
+                            coord += new Vector3(0, 0, 5);
+                        }
+                        else{
+                            coord += new Vector3(0, 0, -5);
+                        }
+                    }
+
+                    dir = Random.Range(-1.0f, 1.0f);
+                    if(dir > 0){
+                        if(dir > .5f){
+                            coord += new Vector3(0, 1, 0);
+                        }
+                        else{
+                            coord += new Vector3(0, -1, 0);
+                        }
+                    }
+
+                    if(!Physics.CheckSphere(coord, 1f)){
+                        availableObjects[nextCloudSlot].transform.parent.parent.SetPositionAndRotation(coord, Quaternion.identity);
+                        nextCloudSlot++;
+                    }
+                }
             }
         }
     }
@@ -83,7 +129,6 @@ public class GameManager : MonoBehaviour
                 if(!Physics.CheckSphere(coord, 1f)){
                     Instantiate(cloudPrefab, coord, Quaternion.identity);
                 }
-                
             }
         }
     }
